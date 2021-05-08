5a3b99af563b4b8ad931a7fb964214fd3551f2f4
Nitin George Cherian <realnitinworks@gmail.com>
2021-05-08 13:42:39 +0530

    feat: Add code and test to get single movie

--- app/movies/views.py
+++ app/movies/views.py
@@ -15,3 +15,15 @@ class MovieList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class MovieDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, pk, format=None):
+        movie = self.get_object(pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
--- app/tests/movies/test_views.py
+++ app/tests/movies/test_views.py
@@ -59,3 +59,18 @@ def test_add_movie_invalid_json_keys(client):
 
     movies = Movie.objects.all()
     assert len(movies) == 0
+
+
+@pytest.mark.django_db
+def test_get_single_movie(client):
+    movie = Movie.objects.create(title="The Big Lebowski", genre="comedy", year="1998")
+    resp = client.get(f"/api/movies/{movie.id}/")
+    assert resp.status_code == 200
+    assert resp.data["title"] == "The Big Lebowski"
+
+
+@pytest.mark.django_db
+def test_get_single_movie_incorrect_id(client):
+    resp = client.get(f"/api/movies/foo/")
+    assert resp.status_code == 404
+
